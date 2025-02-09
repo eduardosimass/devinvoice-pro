7f2f1158b5e239e3b11e904485c255f6c570a5ec
Eduardo Simas <simas.developer@gmail.com>
2025-02-09 01:31:37 -0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom'] // Não inclui React e ReactDOM no bundle final
+    }
+  }
 })
+
+
